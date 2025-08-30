ee9b638df79572b7c651ed46ac05fc246f832f8a
priyabanait <123872080+priyabanait@users.noreply.github.com>
2025-08-31 03:01:43 +0530

    '31-8'

--- src/app/page.js
+++ src/app/page.js
@@ -1255,12 +1255,12 @@ we hope to become your go-to property adviser for life. As we are also part of t
 <div className="hidden md:flex w-full relative  ">
   {/* Box with half-overlap */}
   <div className="absolute top-0 z-10  bg-gray-100 p-6 w-140 border-gray-100  ">
-    <p className="text-3xl font-bold text-gray-800">
+    <p className="text-3xl leading-10 font-bold text-gray-800">
       <span className="text-[rgb(206,32,39,255)]">Join us.</span> Our dynamic energy and innovative spirit bring the best and brightest together.
     </p>
   </div>
 
-  <div className="ml-70 h-screen w-screen relative">
+  <div className="ml-70 h-[80vh] w-screen relative">
     <Image
       src="/4.jpg"
       alt="Full Height Image"
@@ -1270,23 +1270,26 @@ we hope to become your go-to property adviser for life. As we are also part of t
     <div className="absolute inset-0 bg-gray-500/50"></div>
   </div>
 
-  <div className="absolute top-90 z-10 bg-[rgb(206,32,39,255)] text-white flex items-center justify-center w-70 h-70">
+  <div className="absolute top-60 z-10 bg-[rgb(206,32,39,255)] text-white flex items-center justify-center w-70 h-65">
     <p className="text-3xl font-bold text-left">
       Want to be an <br /> AGENT?
     </p>
   </div>
 
-  <div className="absolute top-90 left-70 z-10 bg-white/80 p-6 w-[700px] h-70">
-    <p className="text-3xl mt-14 font-bold text-gray-800 leading-relaxed">
+  <div className="absolute top-60 left-70 z-10 bg-white/80 p-6 w-[700px] h-65">
+    <p className="text-3xl mt-14 ml-8 font-bold text-gray-800 leading-relaxed">
       We offer the greatest rewards for <br />
       <span className="text-[rgb(206,32,39,255)] font-bold">exceptional customer care.</span>
     </p>
 
-    <button className="mt-4 bg-[rgb(206,32,39,255)] text-white px-6 py-3 ml-132  text-base font-semibold flex items-center gap-2">
+    <button className=" bg-[rgb(206,32,39,255)] text-white px-6  py-3 ml-132  text-base font-semibold flex items-center gap-2">
       <span className="whitespace-nowrap">Market Centre Search</span>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" 
+     className="h-4 w-4 transform scale-x-[-1]" 
+     fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+</svg>
     </button>
   </div>
 </div>
